8b669e085e0506f0d22d27a2d2fac6345c82e6af
Nicolas Barone <njb227@cornell.edu>
2020-09-25 17:11:15 -0400

    box mover

---
+++ Assets/boxMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        
+        transform.position += Vector3.right*movement;  
+    }
+}

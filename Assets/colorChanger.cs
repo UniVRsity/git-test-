78dd83b1b2fbe9aea01d76a78d94a927c65dbf72
Nicolas Barone <njb227@cornell.edu>
2020-09-25 16:54:10 -0400

    stuff

---
+++ Assets/colorChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            GetComponent<Renderer>().material.color = new Color(Random.Range(0,1f),Random.Range(0,1f), Random.Range(0,1f));
+        }
+    }
+}
